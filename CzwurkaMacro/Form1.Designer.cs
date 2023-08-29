0a1fe35b0fd20d963a876c3403701d559ff2a8fd
Server <Server@DESKTOP-JBML567>
2023-08-29 21:52:51 +0200

    Dodaj pliki projektów.

---
+++ CzwurkaMacro/Form1.Designer.cs
@@ -0,0 +1,75 @@
+﻿namespace CzwurkaMacro
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Wymagana zmienna projektanta.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Wyczyść wszystkie używane zasoby.
+        /// </summary>
+        /// <param name="disposing">prawda, jeżeli zarządzane zasoby powinny zostać zlikwidowane; Fałsz w przeciwnym wypadku.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Kod generowany przez Projektanta formularzy systemu Windows
+
+        /// <summary>
+        /// Metoda wymagana do obsługi projektanta — nie należy modyfikować
+        /// jej zawartości w edytorze kodu.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.button1 = new System.Windows.Forms.Button();
+            this.textBox1 = new System.Windows.Forms.TextBox();
+            this.SuspendLayout();
+            // 
+            // button1
+            // 
+            this.button1.Location = new System.Drawing.Point(177, 203);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(226, 86);
+            this.button1.TabIndex = 0;
+            this.button1.Text = "start";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // textBox1
+            // 
+            this.textBox1.Font = new System.Drawing.Font("Microsoft Sans Serif", 18.25F);
+            this.textBox1.Location = new System.Drawing.Point(127, 120);
+            this.textBox1.Name = "textBox1";
+            this.textBox1.Size = new System.Drawing.Size(365, 35);
+            this.textBox1.TabIndex = 1;
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(15)))), ((int)(((byte)(15)))), ((int)(((byte)(15)))));
+            this.ClientSize = new System.Drawing.Size(620, 354);
+            this.Controls.Add(this.textBox1);
+            this.Controls.Add(this.button1);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.TextBox textBox1;
+    }
+}
+
---
+++ CzwurkaMacro/Form1.cs
@@ -0,0 +1,79 @@
+﻿using System;
+using System.Runtime.InteropServices;
+using System.Threading;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace CzwurkaMacro
+{
+    public partial class Form1 : Form
+
+    {
+        const int PauseBetweenStrokes = 50;
+        [DllImport("user32.dll", SetLastError = true)]
+        static extern void keybd_event(byte bVk, byte bScan, int dwFlags, int dwExtraInfo);
+
+        const int KEY_DOWN_EVENT = 0x0001; //Key down flag
+        const int KEY_UP_EVENT = 0x0002; //Key up flag
+
+        public static void HoldKey(byte key, int duration)
+        {
+            int totalDuration = 0;
+            while (totalDuration < duration)
+            {
+                keybd_event(key, 0, KEY_DOWN_EVENT, 0);
+                keybd_event(key, 0, KEY_UP_EVENT, 0);
+                System.Threading.Thread.Sleep(PauseBetweenStrokes);
+                totalDuration += PauseBetweenStrokes;
+            }
+        }
+        [DllImport("User32.Dll", EntryPoint = "PostMessageA")]
+        private static extern bool PostMessage(IntPtr hWnd, uint msg, int wParam, int lParam);
+
+        [DllImport("user32.dll", SetLastError = true)]
+        public static extern IntPtr FindWindow(string lpClassName, string lpWindowName);
+
+        const int WM_MOUSELEFTDOWN = 0x201;
+        const int WM_MOUSELEFTUP = 0x202;
+        const int WM_DDOWN = 0x44;
+        const int WM_DUP = 0xC4;
+
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            if (button1.Text == "start")
+            {
+                button1.Text = "stop";
+                kopanie(button1, textBox1);
+            }
+            else if (button1.Text == "stop")
+                button1.Text = "start";
+
+            void kopanie(Button toggle, TextBox oknomc)
+            {
+                
+                while (toggle.Text == "stop")
+                {
+                    IntPtr hwnd = FindWindow(null, oknomc.Text);
+                    PostMessage(hwnd, WM_MOUSELEFTDOWN, 0, 0);
+                    PostMessage(hwnd, WM_DDOWN, 0, 0);
+                    Thread.Sleep(1000);
+                    PostMessage(hwnd, WM_DUP, 0, 0);
+                    Thread.Sleep(10000);
+                    PostMessage(hwnd, WM_MOUSELEFTUP, 0, 0);
+                    Thread.Sleep(1);
+
+                }
+            }
+        }
+    }
+}
